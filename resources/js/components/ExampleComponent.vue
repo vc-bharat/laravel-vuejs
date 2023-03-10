4cf383c75c67032b20a462c64b34a88ade1e94ee
Bharat khairnar <bharat.khairnar@viitor.cloud>
2023-03-10 19:01:47 +0530

    Initial Commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,30 @@
+
+<script>
+    export default {
+        data() {
+            return {
+                message:"Hello Bharat",
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                        {{ message }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <ExampleComponent />
+    <div class="wrapper">
+        <div v-for="airport in airports" :key="airport.abbreviation">
+            <AirportCardComponent :airport="airport"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import allairports from './../data/airports.js';
+import ExampleComponent from './ExampleComponent.vue';
+import AirportCardComponent from './AirportCardComponent.vue';
+export default({
+    name: 'App',
+    components: {
+        ExampleComponent,
+        AirportCardComponent
+    },
+    setup() {
+        const airports = ref(allairports)
+        return { airports }
+    }
+
+})
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 1rem;
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+p,
+h3 {
+  grid-column: span 3;
+}
+</style>
